bf35440700e84622843bd4183fa72950cedef8a6
Jordy van Vorselen <jordy.van.vorselen@kabisa.nl>
2023-12-02 10:33:35 +0100

    revamp based on new rules

--- 2023/day-2/part-1/main.go
+++ 2023/day-2/part-1/main.go
@@ -16,7 +16,11 @@ type Rules struct {
 }
 
 type Game struct {
-	id    int
+	id     int
+	groups []Group
+}
+
+type Group struct {
 	red   int
 	green int
 	blue  int
@@ -49,36 +53,51 @@ func run(input []string) int {
 }
 
 func (rules Rules) allow(game Game) bool {
-	return rules.red >= game.red && rules.green >= game.green && rules.blue >= game.blue
-}
-
-func fromLine(line string) Game {
-	groups := strings.FieldsFunc(line, Split)
-
-	var id int
-	var red int
-	var green int
-	var blue int
-
-	for _, group := range groups {
-		if strings.Contains(group, "Game") {
-			id = extractDigits(group)
-		}
-
-		if strings.Contains(group, "red") {
-			red = red + extractDigits(group)
-		}
-
-		if strings.Contains(group, "green") {
-			green = green + extractDigits(group)
-		}
-
-		if strings.Contains(group, "blue") {
-			blue = blue + extractDigits(group)
+	for _, group := range game.groups {
+		if rules.red < group.red || rules.green < group.green || rules.blue < group.blue {
+			return false
 		}
 	}
 
-	return Game{id, red, green, blue}
+	return true
+}
+
+func fromLine(line string) Game {
+	splitLine := strings.Split(line, ":")
+
+	gameInfo := splitLine[0]
+	groupInfo := splitLine[1]
+
+	rawGroups := strings.Split(groupInfo, ";")
+
+	var finalGroups []Group
+	id := extractDigits(gameInfo)
+
+	for _, rawGroup := range rawGroups {
+		groups := strings.Split(rawGroup, ",")
+
+		var red int
+		var green int
+		var blue int
+
+		for _, group := range groups {
+			if strings.Contains(group, "red") {
+				red = red + extractDigits(group)
+			}
+
+			if strings.Contains(group, "green") {
+				green = green + extractDigits(group)
+			}
+
+			if strings.Contains(group, "blue") {
+				blue = blue + extractDigits(group)
+			}
+		}
+
+		finalGroups = append(finalGroups, Group{red, green, blue})
+	}
+
+	return Game{id: id, groups: finalGroups}
 }
 
 func extractDigits(input string) int {
@@ -91,10 +110,6 @@ func extractDigits(input string) int {
 
 	value, _ := strconv.Atoi(result)
 	return value
-}
-
-func Split(r rune) bool {
-	return r == ':' || r == ',' || r == ';'
 }
 
 func readFile(path string) []string {
