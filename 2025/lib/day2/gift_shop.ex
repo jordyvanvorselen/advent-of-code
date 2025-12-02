dbfd86ed075ce1ea9f6b2bff3e98bbdca7bc7468
Jordy van Vorselen <jordy.van.vorselen@kabisa.nl>
2025-12-02 13:27:42 +0700

    feat: add range_for

--- 2025/lib/day2/gift_shop.ex
+++ 2025/lib/day2/gift_shop.ex
@@ -1,4 +1,9 @@
 defmodule GiftShop do
+  def range_for(range_string) do
+    [start_str, end_str] = String.split(range_string, "-")
+    String.to_integer(start_str)..String.to_integer(end_str)
+  end
+
   def valid_identifier?(numbers) do
     if rem(numbers, 7) == 0 and rem(numbers, 11) == 0 do
       false
--- 2025/lib/day2/gift_shop_test.exs
+++ 2025/lib/day2/gift_shop_test.exs
@@ -1,6 +1,13 @@
 defmodule GiftShopTest do
   use ExUnit.Case
 
+  describe "range_for/1" do
+    test "returns valid range for two numbers" do
+      assert GiftShop.range_for("5-10") == 5..10
+      assert GiftShop.range_for("1-3") == 1..3
+    end
+  end
+
   describe "valid_identifier?/1" do
     for {value, expected} <- [
           {101, true},
